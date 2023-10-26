031fb28c9a3a927e4cc20723fa8a6495e95a65ba
pardeep yadav <pardeepyadav2914@gmail.com>
2023-10-26 21:57:30 +0530

    final commit

--- link_list.py
+++ link_list.py
@@ -50,7 +50,7 @@ class LinkedList:
 
     def prepend(self, value):
         new_node = Node(value)
-        if self.head is Node:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
             self.length = 1
@@ -59,9 +59,74 @@ class LinkedList:
             self.head = new_node
         self.length += 1
 
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
 
-data: LinkedList = LinkedList(10)
-data.append(5)
-data.append(6)
-data.prepend(1)
-print(data.print_list())
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            self.prepend(value)
+            return True
+        if index == self.length:
+            self.append(value)
+            return True
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            return self.head
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
--- test_link_list.py
+++ test_link_list.py
@@ -46,6 +46,63 @@ class TestLinkedList(unittest.TestCase):
         else:
             print(Fore.RED + f"Test Failed: {data.tail.value}" + Style.RESET_ALL)
 
+    def test_pop_first(self):
+        data = LinkedList(10)
+        data.append(5)
+        data.append(16)
+        data.pop_first()
+        if data.head.value == 5:
+            print(Fore.GREEN + f"Test Passed: {data.head.value}" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + f"Test Failed: {data.head.value}" + Style.RESET_ALL)
+
+    def test_get(self):
+        data = LinkedList(10)
+        data.append(5)
+        data.append(16)
+        if data.get(1).value == 5:
+            print(Fore.GREEN + f"Test Passed: {data.get(1).value}" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + f"Test Failed: {data.get(1).value}" + Style.RESET_ALL)
+
+    def test_set_value(self):
+        data = LinkedList(10)
+        data.append(5)
+        data.append(16)
+        data.set_value(0, 99)
+        if data.head.value == 99:
+            print(Fore.GREEN + f"Test Passed: {data.head.value}" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + f"Test Failed: {data.head.value}" + Style.RESET_ALL)
+
+    def test_insert(self):
+        data = LinkedList(10)
+        data.insert(0, 99)
+        if data.head.value == 99:
+            print(Fore.GREEN + f"Test Passed: {data.head.value}" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + f"Test Failed: {data.head.value}" + Style.RESET_ALL)
+
+    def test_remove(self):
+        data = LinkedList(10)
+        data.append(5)
+        data.append(16)
+        data.remove(1)
+        if data.get(1).value == 16:
+            print(Fore.GREEN + f"Test Passed: {data.get(1).value}" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + f"Test Failed: {data.get(1).value}" + Style.RESET_ALL)
+
+    def test_reverse(self):
+        data = LinkedList(10)
+        data.append(5)
+        data.append(16)
+        data.reverse()
+        if data.head.value == 16:
+            print(Fore.GREEN + f"Test Passed: {data.head.value}" + Style.RESET_ALL)
+        else:
+            print(Fore.RED + f"Test Failed: {data.head.value}" + Style.RESET_ALL)
+
 
 if __name__ == "__main__":
     unittest.main()
